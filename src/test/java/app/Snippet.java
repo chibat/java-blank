2797bf479b10f266621dfc0d45d8195b90f92a17
chibat <tomofumi.chiba@gmail.com>
2017-08-15 07:47:41 +0900

    -

--- src/test/java/app/Snippet.java
+++ src/test/java/app/Snippet.java
@@ -9,12 +9,22 @@ import lombok.val;
 public class Snippet {
 
     /**
-     * display system property
+     * display system properties
      */
     @Test
     public void displaySystemProperties() {
         val props = System.getProperties();
         new TreeMap<>(props).forEach((key, value) -> {
+            System.out.println(key + "=" + value);
+        });
+    }
+
+    /**
+     * display environment values
+     */
+    @Test
+    public void displayEnvironmentValues() {
+        new TreeMap<>(System.getenv()).forEach((key, value) -> {
             System.out.println(key + "=" + value);
         });
     }
