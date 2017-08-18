1ebe0e020821dee14d9ebb9fbc04323d75b436be
chibat <tomofumi.chiba@gmail.com>
2017-08-19 01:17:05 +0900

    -

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,6 +1,8 @@
 package app;
 
 import java.io.IOException;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 import lombok.val;
 
@@ -10,8 +12,17 @@ public class Application {
         return "Hello";
     }
 
+    public static String getGitRevision() {
+        try {
+            return ResourceBundle.getBundle("git").getString("git.commit.id");
+        } catch (MissingResourceException e) {
+            return "";
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         val app = new Application();
         System.out.println(app.greet());
+        System.out.println("Git revision: " + getGitRevision());
     }
 }
